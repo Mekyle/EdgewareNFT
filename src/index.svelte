227e9087b993f8afcbe5acf568b4e1e2565121c0
Mekyle <mekylenaidoo@yahoo.com>
2021-03-16 07:16:05 +0200

    Wallet connect button created

--- src/index.svelte
+++ src/index.svelte
@@ -1,24 +1,9 @@
-<div class="p-5 h-screen flex justify-center items-center">
+<script>
+</script>
 
-    <section class="text-2xl text-gray-600">
-        <span class="p-2 bg-red-600 text-white rounded-lg shadow-sm">
-            Svelte
-        </span>
-        {' '}+
-        <span class="p-2 bg-green-600 text-white rounded-lg shadow-sm">
-            Tailwind CSS
-        </span>
-    </section>
-
-    <footer class="flex absolute bottom-0 mb-5 text-gray-700">
-        <p>Powered by</p>
-        <a
-            class="ml-1 text-gray-800 underline"
-            href="https://github.com/davi-mbatista/start"
-            alt="GitHub's start template project"
-            target="_blank"
-            rel="noopener noreferrer">
-            /start
-        </a>
-    </footer>
+<!--This wallet connects to walletconnect and allows a user to log in through whatever login provider they want-->
+<div class="relative">
+    <div class="absolute top-0 right-0">
+        <button class="border-2 px-20 py-4 rounded-full text-white bg-blue-500">Connect wallet</button>
+    </div>
 </div>
