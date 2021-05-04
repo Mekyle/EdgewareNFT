8c7af556acc0b14beaaadba9114237e61d443d56
Mekyle Logen Naidoo <mekylenaidoo@yahoo.com>
2021-05-04 14:19:44 +0200

    UI Update

--- src/index.svelte
+++ src/index.svelte
@@ -1,24 +1,81 @@
-<div class="p-5 h-screen flex justify-center items-center">
-
-    <section class="text-2xl text-gray-600">
-        <span class="p-2 bg-red-600 text-white rounded-lg shadow-sm">
-            Svelte
-        </span>
-        {' '}+
-        <span class="p-2 bg-green-600 text-white rounded-lg shadow-sm">
-            Tailwind CSS
-        </span>
-    </section>
-
-    <footer class="flex absolute bottom-0 mb-5 text-gray-700">
-        <p>Powered by</p>
-        <a
-            class="ml-1 text-gray-800 underline"
-            href="https://github.com/davi-mbatista/start"
-            alt="GitHub's start template project"
-            target="_blank"
-            rel="noopener noreferrer">
-            /start
-        </a>
-    </footer>
+<div class="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5">
+    <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
+        <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-blue-500 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
+            Connect wallet
+        </button>
+    </div>
+    <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style="max-width:1000px">
+        <div class="md:flex w-full">
+            <div class="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
+                <div class="bg-random flex items-center justify-center px-5 py-5">
+                    <div class="w-full mx-auto rounded-3xl shadow-xl bg-white" style="max-width: 300px">
+                        <div class="w-full px-5 pt-8 pb-3">
+                            <div class="rounded-full w-44 h-44 mx-auto border-8 border-gray-300 flex items-center justify-center">
+                                <i class="mdi mdi-arrow-down text-8xl text-gray-300 leading-none"></i>
+                            </div>
+                        </div>
+                        <div class="w-full px-5 text-center pb-8 border-b border-gray-300">
+                            <h1 class="text-2xl font-light text-gray-800 leading-tight">Ready when you are</h1>
+                        </div>
+                        <ul>
+                            <li class="w-full px-5 py-2 border-b border-gray-300">
+                                <div class="w-full flex">
+                                    <div class="flex-grow">
+                                        <h3 class="text-sm text-gray-800">Name: LoremIpsum.zip</h3>
+                                        <p class="text-xs text-gray-500">Size: 138 MB - zip</p>
+                                    </div>
+                                    <div class="w-12 text-right">
+                                        <i class="mdi mdi-download-circle-outline text-blue-500 text-2xl"></i>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="w-full px-5 py-5 text-center">
+                            <button class="bg-blue-500 hover:bg-blue-600 text-white rounded-full px-10 py-2 transition-colors">Choose file</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 py-10 px-5 md:px-10">
+                <div class="text-center">
+                    <h1 class="font-bold text-3xl text-gray-900">Edgeware NFT creator</h1>
+                    <p>Create your own non-fungible token</p>
+                </div>
+                <div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <label for="" class="text-xs font-semibold px-1">Title</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="text-gray-400 text-lg"></i></div>
+                                <input type="text" class="w-full -ml-10 pl-5 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="NFT Name">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <label for="" class="text-xs font-semibold">Description</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                <input type="email" class="w-full -ml-10 pl-5 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Description....">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex -mx-3">
+                        <div class="w-1/3 px-3 mb-12">
+                            <label for="" class="text-xs font-semibold px-1">Quantity</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                <input type="number" class="w-full -ml-10 pl-5 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="0">
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">Deploy NFT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
